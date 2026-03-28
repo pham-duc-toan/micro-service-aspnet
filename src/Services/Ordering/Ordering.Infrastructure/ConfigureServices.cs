20fb6c3029c116d834e02e3821ff75eea90bf57e
tuanphong32 <tuanphong322003@gmail.com>
2026-03-28 22:50:37 +0700

    5.5 Xây dựng Infrastructure Layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,33 @@
+using Contracts.Common.Interfaces;
+using Infrastructure.Common;
+using Infrastructure.Services;
+using Contracts.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<OrderContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"),
+                builder =>
+                    builder.MigrationsAssembly(typeof(OrderContext).Assembly.FullName));
+        });
+
+        services.AddScoped<OrderContextSeed>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+
+        services.AddScoped(typeof(ISmtpEmailService), typeof(SmtpEmailService));
+
+        return services;
+    }
+}
