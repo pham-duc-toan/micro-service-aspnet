677efefa47ac5d591541e1f43d0907eee54722bb
pham-duc-toan <phamductoan220704@gmail.com>
2026-04-21 20:06:53 +0700

    Elastic search: Configure

--- src/BuildingBlocks/Common.Logging/Serilogger.cs
+++ src/BuildingBlocks/Common.Logging/Serilogger.cs
@@ -1,24 +1,37 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Common.Logging
 {
     public static class Serilogger
     {
-        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
-        {
-            var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
-            var environmentName = context.HostingEnvironment.EnvironmentName ?? "Development";
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure = (context, configuration) =>
+            {
+                var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
+                var environmentName = context.HostingEnvironment.EnvironmentName ?? "Development";
+                var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+                var userName = context.Configuration.GetValue<string>("ElasticConfiguration:UserName");
+                var password = context.Configuration.GetValue<string>("ElasticConfiguration:Password");
 
-            configuration
-                .WriteTo.Debug()
-                .WriteTo.Console(outputTemplate:
-                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
-                .Enrich.FromLogContext()
-                .Enrich.WithMachineName()
-                .Enrich.WithProperty("Environment", environmentName)
-                .Enrich.WithProperty("Application", applicationName)
-                .ReadFrom.Configuration(context.Configuration);
-        };
+                configuration
+                    .WriteTo.Debug()
+                    .WriteTo.Console(outputTemplate:
+                        "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+                    {
+                        IndexFormat = $"tedulogs-{applicationName}-{environmentName}-{DateTime.UtcNow:yyyy-MM}",
+                        AutoRegisterTemplate = true,
+                        NumberOfReplicas = 1,
+                        NumberOfShards = 2,
+                        ModifyConnectionSettings = x => x.BasicAuthentication(userName, password)
+                    })
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithProperty("Environment", environmentName)
+                    .Enrich.WithProperty("Application", applicationName)
+                    .ReadFrom.Configuration(context.Configuration);
+            };
     }
 }
