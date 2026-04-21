4606f2dc0ea66947260b9512b03f6dc361210753
pham-duc-toan <phamductoan220704@gmail.com>
2026-04-21 20:06:52 +0700

    Saga: Setup Checkout Service

---
+++ src/Saga.Orchestrator/Saga.Orchestrator/HttpRepository/Interfaces/IInventoryHttpRepository.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DTOs.Inventory;
+
+namespace Saga.Orchestrator.HttpRepository.Interfaces;
+
+public interface IInventoryHttpRepository
+{
+    Task<string> CreateSalesOrderAsync(SalesProductDto model);
+
+    Task<bool> DeleteOrderByDocumentNoAsync(string documentNo);
+}
---
+++ src/Saga.Orchestrator/Saga.Orchestrator/HttpRepository/Interfaces/IOrderHttpRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shared.DTOs.Order;
+
+namespace Saga.Orchestrator.HttpRepository.Interfaces;
+
+public interface IOrderHttpRepository
+{
+    Task<long> CreateOrderAsync(CreateOrderDto order);
+
+    Task<OrderDto> GetOrderAsync(long id);
+
+    Task<bool> DeleteOrderAsync(long id);
+
+    Task<bool> DeleteOrderByDocumentNo(string documentNo);
+}
